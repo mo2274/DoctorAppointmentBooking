5404d35409ab200531c1a2caa962091aaaac76be
mo2274 <mohamed.zaghloul@evapharma.com>
2025-01-23 23:56:46 +0200

    Add SlotId to appointment

--- AppointmentBooking.Infrastructure/Database/Migrations/20250123215252_add slot id to appointment.Designer.cs
+++ AppointmentBooking.Infrastructure/Database/Migrations/20250123215252_add slot id to appointment.Designer.cs
@@ -4,6 +4,7 @@ using AppointmentBooking.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppointmentBooking.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AppointmentBookingDbContext))]
-    partial class AppointmentBookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250123215252_add slot id to appointment")]
+    partial class addslotidtoappointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AppointmentBooking.Infrastructure/Database/Migrations/20250123215252_add slot id to appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentBooking.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class addslotidtoappointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SlotId",
+                schema: "AppointmentBookings",
+                table: "Appointments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SlotId",
+                schema: "AppointmentBookings",
+                table: "Appointments");
+        }
+    }
+}
